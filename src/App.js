751bc1efbb89ae3f0168a885689fafdd0a649fc7
isabroch <isabrochweb@gmail.com>
2020-09-03 11:46:17 +0200

    Register service worker for notifications

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ import { motion } from "framer-motion";
 
 import { PomodoroTimer } from "./PomodoroTimer";
 
-// Inititate service worker!
+// Inititate service worker
+// store registration data for push notifs
+let registration = null;
+
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", async function () {
     try {
-      const registration = await navigator.serviceWorker.register("/sw.js");
+      registration = await navigator.serviceWorker.register("/sw.js");
       console.log(`Created service worker with scope: ${registration.scope}`);
     } catch (error) {
       console.error(`Failed to create service worker: ${error}`);
